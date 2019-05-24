86f3f0afe88597b759e089e65c8bd260316d91fa
mouhxingxiaoxia <13120950792qq.com>
2019-05-24 09:59:43 +0800

    修改了Disease的结构

--- src/main/java/com/neuedu/hospitalinformationsystem/po/Disease.java
+++ src/main/java/com/neuedu/hospitalinformationsystem/po/Disease.java
@@ -1,61 +1,24 @@
 package com.neuedu.hospitalinformationsystem.po;
 
 public class Disease {
-    private String disId;
-    private String dcId;
-    private String disName;
-    private String disCode;
+    /*自动生成主键*/
+    private Integer disId;
+/*疾病完整编码*/
     private String disIcd;
+/*疾病完整名称*/
+    private String disName;
+//拼音首拼
+    private String disCode;
+//疾病种类
+    private String disType;
 
-    @Override
-    public String toString() {
-        return "Disease{" +
-                "disId='" + disId + '\'' +
-                ", dcId='" + dcId + '\'' +
-                ", disName='" + disName + '\'' +
-                ", disCode='" + disCode + '\'' +
-                ", disIcd='" + disIcd + '\'' +
-                '}';
-    }
 
-    public String getDcId() {
-        return dcId;
-    }
-
-    public void setDcId(String dcId) {
-        this.dcId = dcId;
-    }
-
-    public String getDisCode() {
-        return disCode;
-    }
-
-    public void setDisCode(String disCode) {
-        this.disCode = disCode;
-    }
-
-    public String getDisId() {
+    public Integer getDisId() {
         return disId;
     }
 
-    public void setDisId(String disId) {
+    public void setDisId(Integer disId) {
         this.disId = disId;
-    }
-
-    public String getDisName() {
-        return disName;
-    }
-
-    public void setDisName(String disName) {
-        this.disName = disName;
-    }
-
-    public String getDisSpell() {
-        return disCode;
-    }
-
-    public void setDisSpell(String disSpell) {
-        this.disCode = disSpell;
     }
 
     public String getDisIcd() {
@@ -66,4 +29,27 @@ public class Disease {
         this.disIcd = disIcd;
     }
 
+    public String getDisName() {
+        return disName;
+    }
+
+    public void setDisName(String disName) {
+        this.disName = disName;
+    }
+
+    public String getDisCode() {
+        return disCode;
+    }
+
+    public void setDisCode(String disCode) {
+        this.disCode = disCode;
+    }
+
+    public String getDisType() {
+        return disType;
+    }
+
+    public void setDisType(String disType) {
+        this.disType = disType;
+    }
 }
